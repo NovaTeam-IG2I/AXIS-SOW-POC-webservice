d2d2d6e13c82fad3b4a17da83f1b201bb2bdfccf
Richard Degenne <richdeg2@gmail.com>
2016-11-23 12:23:50 +0100

    Implemented file upload
    The `importServlet` is now able to handle HTTP requests for file uploads.
    
    A sample web page `import.html` allows anybody to test the servlet.
    
    Documentation has also been added.

--- AXIS-SOW-POC-backend/src/main/java/rocks/novateam/axis/sow/poc/backend/servlets/importServlet.java
+++ AXIS-SOW-POC-backend/src/main/java/rocks/novateam/axis/sow/poc/backend/servlets/importServlet.java
@@ -1,22 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rocks.novateam.axis.sow.poc.backend.servlets;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.nio.file.Paths;
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import org.apache.commons.io.IOUtils;
 
 /**
- *
- * @author richou
+ * This servlet handles media importation.
+ * 
+ * The HTTP request @b must
+ *     + Have a <code>multipart/form-data</code> MIME type ;
+ *     + Contain a field named <code>file</code> containing the file to import.
+ * 
+ * The HTTP response will have a <code>application/json</code> MIME type, and may contain:
+ *     + <code>{'status': 'ok', 'filename': <em>filename</em>}</code> if the request succeeded ;
+ *     + <code>{'status': 'ko', 'message': <em>message</em>}</code> if the request failed.
+ * 
+ * @author Richard Degenne
  */
+@MultipartConfig(location="E:\\Users\\richou\\Desktop") // This folder is for temporary storage ONLY.
 public class importServlet extends HttpServlet {
+
+    /**
+     * Target folder for the uploaded files.
+     */
+    public static String UPLOAD_FOLDER = "E:\\Users\\richou\\Desktop\\";
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -29,18 +49,34 @@ public class importServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        JsonObjectBuilder json = Json.createObjectBuilder();
+        try {
+            // Get data from the request object
+            Part filePart = request.getPart("file");
+            if(filePart == null) {
+                throw new FileNotFoundException("No 'file' part given in the request.");
+            }
+            String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // This is an IE fix.
+            InputStream fileContent = filePart.getInputStream();
+
+            // Copy media to disk
+            File file = new File(UPLOAD_FOLDER+fileName);
+            file.createNewFile();
+            FileOutputStream fileOutputStream = new FileOutputStream(file, false); // TODO: Have an incremental id for file names.
+            IOUtils.copy(fileContent, fileOutputStream);
+            
+            json.add("status", "ok")
+                    .add("filename", fileName);
+        }
+        catch(FileNotFoundException e) {
+            json.add("status", "ko")
+                    .add("message", e.getMessage());
+        }
+        
+        // Send response
+        response.setContentType("application/json");
         try(PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet importServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet importServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            out.println(json.build().toString());
         }
     }
 
@@ -80,7 +116,7 @@ public class importServlet extends HttpServlet {
      */
     @Override
     public String getServletInfo() {
-        return "Short description";
+        return "This servlet handles media importation.";
     }// </editor-fold>
 
 }
