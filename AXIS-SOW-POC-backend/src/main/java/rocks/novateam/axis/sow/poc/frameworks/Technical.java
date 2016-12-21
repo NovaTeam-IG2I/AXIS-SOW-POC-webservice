c90dc96e3247dd224df2a6119eaa9ece705e4a9c
Alex Canales <example@example.com>
2016-12-21 14:16:19 +0100

    Add data test insertion.

--- AXIS-SOW-POC-backend/src/main/java/rocks/novateam/axis/sow/poc/frameworks/Technical.java
+++ AXIS-SOW-POC-backend/src/main/java/rocks/novateam/axis/sow/poc/frameworks/Technical.java
@@ -1,6 +1,12 @@
 package rocks.novateam.axis.sow.poc.frameworks;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import rocks.novateam.axis.sow.poc.backend.Configuration;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.util.FileManager;
 
 /**
  * This class holds all informations about the technical framework.
@@ -11,9 +17,9 @@ import java.util.ArrayList;
  * @author alex
  */
 public class Technical {
-    
+
     private String fileName = "";
-    
+
     /**
      * Fills the object with fake data: DELETE THIS FUNCTION.
      */
@@ -36,5 +42,33 @@ public class Technical {
     public String exportJSONFormat() {
         // TODO
         return " { fileName : \"" + fileName + "\" }";
+    }
+
+    public static void main(String[] args) throws IOException {
+        // URIs and SPARQL prefix, should be moved in a special class
+        final String POC_URI = "http://axis.sow/poc/";
+        final String DATAMODEL_URI = "http://titan.be/axis-csrm/datamodel/ontology/0.4#";
+        final String RDF_URI = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+        final String RDFS_URI = "http://www.w3.org/2000/01/rdf-schema#";
+        final String PREFIX = "PREFIX rdfs: <" + RDFS_URI + ">\n" +
+                "PREFIX rdf: <" + RDF_URI + ">\n" +
+                "PREFIX datamodel: <" + DATAMODEL_URI + ">\n" +
+                "PREFIX poc: <" + POC_URI + ">\n";  // NOTE: Have to be decided
+
+        // Data test definition
+        String filmId = "Selma";
+        String fileName = "Selma.mp4";
+
+        // Inserting data in the model
+        Model model = FileManager.get().loadModel(
+                Configuration.getInstance().getDatamodelFile(), null, "TURTLE"
+        );
+        Resource selma = model.createResource(POC_URI + filmId);
+        Property isA = model.createProperty(RDF_URI + "type");
+        selma.addProperty(isA, DATAMODEL_URI + "AudiovisualWork");
+        Property fileNamePro = model.createProperty(DATAMODEL_URI + "FileName");
+        selma.addProperty(fileNamePro, fileName);
+
+        // model.write(System.out);  // Printing the data (should be saved)
     }
 }
