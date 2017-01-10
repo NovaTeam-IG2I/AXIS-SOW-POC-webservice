6d9d3f444112d7c28b5fc843138eee416994fbee
Melody <ashiryanerelfend@gmail.com>
2017-01-10 10:15:12 +0100

    Create class Event with properties

---
+++ AXIS-SOW-POC-backend/src/main/java/rocks/novateam/axis/sow/poc/model/Event.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rocks.novateam.axis.sow.poc.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Mélody
+ */
+public class Event {
+    public Date dateOfEvent;
+    public String placeOfEvent;
+    public String description;
+    public String uri;
+    public String type;
+    
+    
+}
