4a676ceb32a1fd06af6716e22bb256ad4b790636
Alex Canales <example@example.com>
2017-01-11 14:59:16 +0100

    Create a Person register

---
+++ AXIS-SOW-POC-backend/src/main/java/rocks/novateam/axis/sow/poc/backend/model/Person.java
@@ -0,0 +1,36 @@
+package rocks.novateam.axis.sow.poc.backend.model;
+
+import java.io.IOException;
+import rocks.novateam.axis.sow.poc.backend.R;
+
+/**
+ * Defines a person.
+ *
+ * @author Alex Canales
+ */
+public class Person extends Register {
+    
+    /**
+     * The person type.
+     */
+    public static final String TYPE = "person";
+
+    public Person(String uri) {
+        this.uri = uri;
+    }
+
+    @Override
+    public String toJSON() {
+        String json = "{\n";
+        json += "\"uri\" : \"" + uri + "\",\n";
+        json += "\"label\" : \"" + label + "\",\n";
+        json += "}";
+        return json;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String personID = R.POC_NS + "MLK";
+        Register person = RegisterFactory.getInstance("person", personID);
+        System.out.println(person.toJSON());
+    }
+}
