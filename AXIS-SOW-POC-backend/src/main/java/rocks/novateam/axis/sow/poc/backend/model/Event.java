b43350ad85b1e09ce5e60df0ecd57b86e9b67cf7
Melody <ashiryanerelfend@gmail.com>
2017-01-12 13:17:47 +0100

    Add constructor in Event

--- AXIS-SOW-POC-backend/src/main/java/rocks/novateam/axis/sow/poc/backend/model/Event.java
+++ AXIS-SOW-POC-backend/src/main/java/rocks/novateam/axis/sow/poc/backend/model/Event.java
@@ -3,20 +3,52 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rocks.novateam.axis.sow.poc.model;
+package rocks.novateam.axis.sow.poc.backend.model;
 
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import rocks.novateam.axis.sow.poc.backend.ontology.RegisterManager;
+import rocks.novateam.axis.sow.poc.backend.ontology.TDBManager;
 
 /**
  *
  * @author Mélody
  */
-public class Event {
-    public Date dateOfEvent;
+public class Event extends Register{
+    private static final String NS = TDBManager.DATAMODEL_NS;
+    
+    public String date;
     public String placeOfEvent;
     public String description;
-    public String uri;
     public String type;
+
+    @Override
+    public String toJSON() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Register getInstance(String uri) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Event(String uri) {
+        RegisterManager rm = new RegisterManager();
+        Map<String,String> map = new HashMap<>();
+        map = rm.getPropertiesValuesOfAnIndividual(uri);
+
+    }
+    
+    //public getProperty
+  
+    public static void main(String[] args) {
+        Map<String,String> properties = new HashMap<>();
+        properties.put("prop1","value1");
+        RegisterManager rm = new RegisterManager();
+        //need to be remove, useful just for a test
+        rm.addRegisterInstance("melo", "Event", properties);
+        Event e = new Event(NS+"melo");
+    }
     
     
 }
