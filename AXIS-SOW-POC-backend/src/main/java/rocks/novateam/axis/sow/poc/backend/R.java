723c336104c0cc425f244ba6a4918fd05b6ce3cb
Geoffrey Lemaire <geoffrey.lemaire@hotmail.fr>
2017-01-12 11:23:18 +0100

    added various namespaces

--- AXIS-SOW-POC-backend/src/main/java/rocks/novateam/axis/sow/poc/backend/R.java
+++ AXIS-SOW-POC-backend/src/main/java/rocks/novateam/axis/sow/poc/backend/R.java
@@ -44,13 +44,31 @@ public class R {
      */
     public static final String XSD_NS = "http://www.w3.org/2001/XMLSchema#";
 
-
     /**
      * The prefix used in every SPARQL requests.
      */
-    public static final String PREFIX = "PREFIX rdfs: <" + RDFS_NS + ">\n" +
-            "PREFIX rdf: <" + RDF_NS + ">\n" +
-            "PREFIX datamodel: <" + DATAMODEL_NS + ">\n" +
-            "PREFIX cidoc: <" + CIDOC_NS + ">\n" +
-            "PREFIX poc: <" + POC_NS + ">\n";
+    public static final String PREFIX = "PREFIX rdfs: <" + RDFS_NS + ">\n"
+            + "PREFIX rdf: <" + RDF_NS + ">\n"
+            + "PREFIX datamodel: <" + DATAMODEL_NS + ">\n"
+            + "PREFIX cidoc: <" + CIDOC_NS + ">\n"
+            + "PREFIX poc: <" + POC_NS + ">\n";
+
+    /**
+     * The namespaces below can be found in some XMP data
+     */
+    public static final String XMP_NS = "http://ns.adobe.com/xap/1.0/";
+    public static final String XMPDM_NS = "http://ns.adobe.com/xmp/1.0/DynamicMedia/";
+    public static final String STDIM_NS = "http://ns.adobe.com/xap/1.0/sType/Dimensions#";
+    public static final String XMPMM_NS = "http://ns.adobe.com/xap/1.0/mm/";
+    public static final String STEVT_NS = "http://ns.adobe.com/xap/1.0/sType/ResourceEvent#";
+    public static final String STREF_NS = "http://ns.adobe.com/xap/1.0/sType/ResourceRef#";
+    public static final String BEXT_NS = "http://ns.adobe.com/bwf/bext/1.0/";
+    public static final String TMRUSH_NS = "http://ns.adobe.com/dva/dynamic/TM-Rush/1.0/";
+    public static final String DC_NS = "http://purl.org/dc/elements/1.1/";
+    public static final String PHOTOSHOP_NS = "http://ns.adobe.com/photoshop/1.0/";
+    public static final String TIFF_NS = "http://ns.adobe.com/tiff/1.0/";
+    public static final String EXIF_NS = "http://ns.adobe.com/exif/1.0/";
+    public static final String CREATORATOM_NS = "http://ns.adobe.com/creatorAtom/1.0/";
+    public static final String TMFILMV2_NS = "http://ns.adobe.com/dva/dynamic/TM-Film-v2/1.0/";
+
 }
