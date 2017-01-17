8f5c8e9caeb3e8f9a891b6d3d0e6535589822f7d
Richard-Degenne <richdeg2@gmail.com>
2017-01-17 20:25:37 +0100

    Implemented track creation
    *Warning*: The newly-created track doesn't have an `ESOStructure` and
    will break the system for the time being.

--- AXIS-SOW-POC-backend/src/main/java/rocks/novateam/axis/sow/poc/backend/servlets/TrackServlet.java
+++ AXIS-SOW-POC-backend/src/main/java/rocks/novateam/axis/sow/poc/backend/servlets/TrackServlet.java
@@ -7,10 +7,24 @@ package rocks.novateam.axis.sow.poc.backend.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.NoSuchElementException;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import rocks.novateam.axis.sow.poc.backend.ontology.TDBManager;
 
 /**
  *
@@ -29,19 +43,33 @@ public class TrackServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet TrackServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet TrackServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String uri = request.getParameter("uri");
+        String name = request.getParameter("name");
+        
+        if(uri==null || name==null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
         }
+        
+        JsonObjectBuilder json = Json.createObjectBuilder();
+        try {
+            Individual indexedTrack = createIndexedTrack(uri, name);
+            
+            // Build the response object
+            json.add("uri", indexedTrack.getURI());
+            json.add("status", "ok");
+        }
+        catch(NoSuchElementException ex) {
+            json.add("status", "ko")
+                    .add("message", ex.getMessage());
+        }
+        
+        // Send response
+        response.setContentType("application/json");
+        try(PrintWriter out = response.getWriter()) {
+            out.println(json.build().toString());
+        }
+            
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -82,5 +110,40 @@ public class TrackServlet extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+    private Individual createIndexedTrack(String uri, String name) throws NoSuchElementException {
+        String NS = TDBManager.DATAMODEL_NS;
+
+        Dataset dataset = TDBManager.getInstance().getDataset();
+        dataset.begin(ReadWrite.WRITE);
+        OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, dataset.getDefaultModel());
+        
+        Individual film = model.getIndividual(uri);
+        if(film == null) {
+            throw new NoSuchElementException("The requested URI does not exist.");
+        }
+        
+        OntClass indexedTrackClass = model.getOntClass(NS+"EditingTrack");
+        Individual track = indexedTrackClass.createIndividual(NS+getNextName(model, name));
+        Property usesProperty = model.getProperty(NS+"uses");
+        film.addProperty(usesProperty, track);
+        dataset.commit();
+        
+        return track;
+    }
+
+    private String getNextName(OntModel model, String name) {
+        String NS = TDBManager.DATAMODEL_NS;
+        Individual individual = model.getIndividual(NS+name);
+        if(individual == null) {
+            return name;
+        }
+        int i=1;
+        while(individual != null) {
+            individual = model.getIndividual(NS+name);
+            i++;
+        }
+        return name+i;
+    }
 
 }
