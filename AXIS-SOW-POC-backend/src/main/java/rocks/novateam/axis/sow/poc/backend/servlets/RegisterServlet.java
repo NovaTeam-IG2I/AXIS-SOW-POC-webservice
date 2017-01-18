21b17379f76c98d84b8ea2979aced37e141b9507
Olivier Sailly <olivier.sailly.59@gmail.com>
2017-01-18 14:45:12 +0100

    Small fix on RegisterServlet

--- AXIS-SOW-POC-backend/src/main/java/rocks/novateam/axis/sow/poc/backend/servlets/RegisterServlet.java
+++ AXIS-SOW-POC-backend/src/main/java/rocks/novateam/axis/sow/poc/backend/servlets/RegisterServlet.java
@@ -64,8 +64,11 @@ public class RegisterServlet extends HttpServlet {
                     out.print(this.processGetAllIndividuals());
                 break;
                 case "properties":
-                    if(uriFields.size() > 1) out.print(this.processGetProperties(uriFields.get(1)));
-                    else response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                    if(!request.getParameterMap().isEmpty()) out.print(this.processGetCategories(request.getParameter("classuri")));
+                    else {
+                        if(uriFields.size() > 1) out.print(this.processGetPropertiesByName(uriFields.get(1)));
+                        else response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                    }
                 break;
                 case "add":
                     if(uriFields.size() > 1) {
@@ -167,13 +170,17 @@ public class RegisterServlet extends HttpServlet {
     private String processGetCategories(String classURI){
         return this.mGson.toJson(this.mRegisterManager.getCategoriesRecusively(classURI), mCategoriesListType.getType());
     }
+    
+    private String processGetProperties(String classURI){
+        return this.mGson.toJson(this.mRegisterManager.getClassPropertiesLocalNames(classURI), mStringListType.getType());
+    }
 
-    private String processGetProperties(String className){
+    private String processGetPropertiesByName(String className){
         return this.mGson.toJson(this.mRegisterManager.getClassPropertiesLocalNamesByClassName(className), mStringListType.getType());
     }
 
     private String processGetAllIndividuals(){
-        return this.mGson.toJson(this.mRegisterManager.getAllIndividuals(), mStringListType.getType());
+        return this.mGson.toJson(this.mRegisterManager.getAllIndividualsLocalNames(), mStringListType.getType());
     }
 
     private boolean processAddRegisterInstance(String name, String classURI, String jsonProperties){
